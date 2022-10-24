022c3a9752d87028accb1266a21b5c474d5cc4fc
AhmedS12899 <AhmedS12899@Gmail.com>
2022-10-24 13:32:48 -0400

    completed functions exercise that accept parameters

---
+++ Functions/more_functions.js
@@ -0,0 +1,17 @@
+function displayMailingLabel(name, address, city, state, zip){
+    console.log(name +  "\n" + address + city + "\n" +  state +  ", " + zip);
+}
+
+function addNumbers(num1, num2){
+    console.log(num1 + " + " + num2 + " = " + Number(num1+num2));
+}
+
+function displayReceipt(totalDue, amountPaid){
+    console.log("Total Due: $" + totalDue + "\n" + "Amount Paid: $" + amountPaid + "\n" + "Change Due: $" + Number(amountPaid-totalDue));
+}
+
+displayMailingLabel("Ahmed Sharhan" , "86 Wall Street", "NYC", "NY", 11235 );
+
+addNumbers(8 , 24);
+
+displayReceipt(14.77, 20.00);
