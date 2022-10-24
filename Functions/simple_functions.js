03a9b7c83664a3740d6738cabcaf3124e515f24a
AhmedS12899 <AhmedS12899@Gmail.com>
2022-10-24 13:13:38 -0400

    defined three basic functions and called them

---
+++ Functions/simple_functions.js
@@ -0,0 +1,15 @@
+function favoriteThings(){
+    console.log("Ahmed Sharhan \nScary Movie \nMichael Jackson");
+}
+
+function whyImHere(){
+    console.log("I joined learning to code because i wanted to understand how to become a full stack developer and learn first hand from professionals in the field, rather than a professor who hasnt been in the field for decades.")
+}
+
+function favoritePlaces(){
+    console.log("One of my favorite places to visit is the basketball courts because i grew up playing basketball and it shaped my life.");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlaces();
